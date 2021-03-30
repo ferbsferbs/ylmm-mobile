642191c70d36c7d0e753eae09cb457a808f2e100
ferbsferbs <fer.lopes.correa@gmail.com>
2021-03-30 15:11:48 -0300

    ui changes

---
+++ ylmm/src/components/FloatingActionButton/index.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+import { connect } from 'react-redux'
+
+
+const FloatingActionButton = (props) => {
+    
+    return (
+        <View onTouchEndCapture={()=>alert(props.accountType)} style={{position:'absolute',bottom:10,left:10,backgroundColor:'red',width:50,height:50,elevation:10,borderRadius:100}}>
+          
+        </View>
+    )
+}
+
+
+const mapStateToProps = (state) => {
+  const {AuthReducer} = state;
+  return {
+    accountType: AuthReducer.accountType,
+
+  
+  };
+};
+const mapDispatchToProps = (dispatch) => {
+  return {};
+};
+export default connect(mapStateToProps, mapDispatchToProps)(FloatingActionButton);
